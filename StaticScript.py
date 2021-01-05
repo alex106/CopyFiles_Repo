d33acbb0e42d853584930afed1d175de028cafe1
Alex Postolov <alex.postolov@gmail.com>
2021-01-05 17:38:17 +0200

    Naming convention

--- StaticScript.py
+++ StaticScript.py
@@ -2,15 +2,15 @@ from pathlib import Path
 import sys, getopt
 from shutil import copyfile
 
-startDir: str
-copyDir: str
-
 
 def main(argv):
     val = 1
+    sourceDir = ""
+    destDir = ""
+    LookUpExtention = []
     if len(argv) >= 3:
-        startDir = str(argv[0])  # "\\ADC_IoT\\GenXStaticLibrary"
-        copyDir = str(argv[1])  # "\\ADC_IoT\\ADCN_Proj"
+        sourceDir = str(argv[0]).strip()  # "\\ADC_IoT\\GenXStaticLibrary"
+        destDir = str(argv[1]).strip()  # "\\ADC_IoT\\ADCN_Proj"
         LookUpExtention = argv[2:]
     else:
         print("Wrong number of parameters")
@@ -18,15 +18,16 @@ def main(argv):
     copyFileCounter = 0
     PathCounter = 0
     directories = []
-    path = Path(startDir).resolve()
+    path = Path(sourceDir).resolve()
     print("The source path: " + str(path))
     copyPath = ""
-    if not Path(copyDir).exists():
-        Path(copyDir).mkdir()
+    if not Path(destDir).exists():
+        Path(destDir).mkdir()
+    print("The destanation path: " + destDir)
     while val == 1:
         # iterate on all files to find
         for file in path.iterdir():
-            copyPath = str(file.resolve()).replace(startDir, copyDir)
+            copyPath = str(file.resolve()).replace(sourceDir, destDir)
             if file.is_dir():
                 if copyPath.find(".") == -1:
                     # save directory full path
@@ -34,17 +35,17 @@ def main(argv):
                     if not Path(copyPath).exists():
                         # Create directory in copy folder
                         Path(copyPath).mkdir()
-                        # print("Create path: " + copyPath)
+                        print("Create path: " + copyPath)
             else:
                 doCopy = False
                 for ext in LookUpExtention:
-                    if copyPath.find(str(ext)) != -1:
+                    if copyPath.find(str(ext).strip()) != -1:
                         doCopy = True
                         break
                 if doCopy == True:
                     copyFileCounter += 1
                     copyfile(str(file.resolve()), copyPath)
-                # print("Copy File from " + str(file.resolve()) + " to " + copyPath)
+                    print("Copy File from " + str(file.resolve()) + " to " + copyPath)
         if len(directories) > 0:
             file = directories[0]
 
@@ -60,7 +61,7 @@ def main(argv):
                 + str(copyFileCounter)
                 + " files was copied"
             )
-            remove_empty_dir(copyDir)
+            remove_empty_dir(destDir)
             val = 0
 
 
@@ -75,6 +76,8 @@ def remove_empty_dir(directory):
         else:
             if not remove_empty_dir(str(file.resolve())):
                 Path(file).rmdir()
+            else:
+                isNotEmpty = True
     return isNotEmpty
 
 
